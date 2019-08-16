669fb570247528c0de3a0372aae494d0eef1fff8
Joe S <joe@skrd.uk>
2019-08-16 11:18:18 +0100

    Add unit conversion static methods

--- tests/PageTest.php
+++ tests/PageTest.php
@@ -34,4 +34,49 @@ class PageTest extends TestCase
 
         $this->assertSame(3579, $page->getImageHeight());
     }
+
+    public function testMillimeterToCentimeter()
+    {
+        $this->assertSame(.1, Page::millimeterToCentimeter(1));
+    }
+
+    public function testMillimeterToInch()
+    {
+        $this->assertSame(.0393701, Page::millimeterToInch(1));
+    }
+
+    public function testMillimeterToPixel()
+    {
+        $this->assertSame(12, Page::millimeterToPixel(1, 118.11023622));
+    }
+
+    public function testCentimeterToMillimeter()
+    {
+        $this->assertSame(10.0, Page::centimeterToMillimeter(1));
+    }
+
+    public function testCentimeterToInch()
+    {
+        $this->assertSame(0.393701, Page::centimeterToInch(1));
+    }
+
+    public function testCentimeterToPixel()
+    {
+        $this->assertSame(118, Page::centimeterToPixel(1, 118.11023622));
+    }
+
+    public function testInchToMillimeter()
+    {
+        $this->assertSame(25.4, Page::inchToMillimeter(1));
+    }
+
+    public function testInchToCentimeter()
+    {
+        $this->assertSame(2.54, Page::inchToCentimeter(1));
+    }
+
+    public function testInchToPixel()
+    {
+        $this->assertSame(300, Page::inchToPixel(1, 118.11023622));
+    }
 }
